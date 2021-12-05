06e342b1d3e57edd72369824a799faeec1cad484
nstarke <starke.nicholas@gmail.com>
2021-12-05 11:14:19 -0600

    Adding map scripts

---
+++ scripts/map-functions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# find . -name "*.map.txt" -type f -exec python3 scripts/map-functions.py {} \;
+import sys, re, copy, functools
+
+MAP_FILE = sys.argv[1]
+
+
+prog = re.compile(r'0x([a-fA-F0-9]{8})$')
+map_file = open(MAP_FILE, 'r')
+lines = map_file.readlines()
+map_file.close()
+
+output = [("", False)] * len(lines)
+
+xref_search = copy.deepcopy(lines)
+
+for idx, line in enumerate(lines):
+    orig_str = line
+    orig_addr = orig_str.split(':')[0]
+    if 'XREF:' not in orig_str and re.search(prog, orig_str):
+        orig_xref = orig_str[-11:].strip()
+        for xref_idx, xref in enumerate(xref_search):
+            xref_str = xref.strip()
+            xref_addr = xref_str.split(':')[0]
+            if xref_addr == orig_xref:
+                f_ref = "\t\t\t\t; FREF: " + orig_addr + '\n'
+                output[xref_idx] = (xref_str + f_ref, True)
+                break
+    
+    if not output[idx][1]:  
+        output[idx] = (orig_str, False)
+
+def mapper(n):
+    return n[0]
+
+open(MAP_FILE, 'w').write(''.join(map(mapper, output)))
--- scripts/map-registers.sh
+++ scripts/map-registers.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 FILENAME="$(pwd)/$1"
-sed -i -e "s/r25/%sp/g" "$FILENAME"
-sed -i -e "s/r26/%lr/g" "$FILENAME"
-sed -i -e "s/r28/%esp/g" "$FILENAME"
-sed -i -e "s/r29/%tp/g" "$FILENAME"
-sed -i -e "s/r30/%sr/g" "$FILENAME"
-sed -i -e "s/r31/%pc/g" "$FILENAME"
+sed -i -e "s/r28/esp/g" "$FILENAME"
+sed -i -e "s/r29/tp/g" "$FILENAME"
+sed -i -e "s/r30/sr/g" "$FILENAME"
